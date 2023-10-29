a4eace2431b898d646a3f26a632b14fc20605813
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-10-29 15:19:34 -0300

    feat: Criação loading para home com skeleton screen

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,5 @@
 import { LayoutParams } from '@/app/common/layout.type';
-import { Header } from '../components/header';
+import { Header } from '@/components/header';
 
 export default function StoreLayout({ children }: LayoutParams) {
   return (
